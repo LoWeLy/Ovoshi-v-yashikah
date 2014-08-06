5a4b4c829f9cd3d8eb2b443fa6115e15295bfc42
Anton Plebanovich <LoWeLy@Antons-Mac-Pro.local>
2014-08-06 06:54:44 +0300

    Initial Commit

---
+++ Ovoshi v yashikah/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Ovoshi v yashikah
+//
+//  Created by Anton on 8/6/14.
+//  Copyright (c) 2014 g12-Squad. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
